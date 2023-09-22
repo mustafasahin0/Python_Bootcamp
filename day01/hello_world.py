c6fbac52f65ea1d8fc0d6fbdcf20e4b7c9781f74
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-21 21:11:51 -0600

    added variable.py

--- day01/hello_world.py
+++ day01/hello_world.py
@@ -2,3 +2,9 @@ print("Hello World")  # This is print statement
 """
 This is multiline comment
 """
+print("Harward\nUniversity")
+print("\tHarward University")
+print("\\")
+print('I love "Python"')
+print("I love \"Python\"")
+print("I love 'Python'")
---
+++ day01/variable.py
@@ -0,0 +1,31 @@
+"""
+In Java:static Typing
+    DataType var = Data
+
+In Python: Dynamic Typing
+    var = Data
+"""
+
+name = None
+print(name)
+name = "James"
+print(name)
+print(type(name))
+
+age = 25
+print(type(age))
+
+gpa = 2.5
+print(type(gpa))
+
+is_employeed = True
+print(type(is_employeed))
+
+# Constructor Function
+s = '25'
+# str(s)
+s = int(s)
+print(s * 5)
+
+
+
