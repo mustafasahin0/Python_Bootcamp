dc6a01579741fc1a74004c1a43f27ac28bba36a1
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-09-24 10:42:59 -0600

    concationation added.

---
+++ day01/concatenation.py
@@ -0,0 +1,10 @@
+name = 'James'
+age = 26
+print("My name is " + name)
+print()
+# print("My age is " + age)
+print('My age is {}'.format(age))
+print('My name is {} and I am {} years old'.format(name, age))
+print(f'My name is {name} and I am {age} years old.')
+
+print('Python', 3, 'is awesome:', True)
